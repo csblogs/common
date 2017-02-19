756e991ab2a46c61736959cbc5580d28d1017ab5
Daniel Brown <d.t.brown@outlook.com>
2017-02-19 19:43:06 +0000

    Export Organisation Model

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import {initDbLogger, database } from './database';
 import User from './database/user-model';
-import BlogPost from './database/post-model';
 import UserSchema from './database/user-schema';
+import BlogPost from './database/post-model';
+import Organisation from './database/organisation-model';
 
 export {
   initDbLogger,
   database,
   User,
   UserSchema,
-  BlogPost
+  BlogPost,
+  Organisation
 };
